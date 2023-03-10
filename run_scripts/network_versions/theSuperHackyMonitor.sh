a8c9e54701a75de21ada6c89bd1484ff76cf6a81
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2023-03-09 18:03:20 -1000

    Update theSuperHackyMonitor.sh

--- run_scripts/network_versions/theSuperHackyMonitor.sh
+++ run_scripts/network_versions/theSuperHackyMonitor.sh
@@ -1,2 +1,2 @@
-while true; do echo -n `ps -eo %cpu,pid --sort -%cpu | awk  '{if($1>90)print}' | wc -l`; sleep 1; echo -en "\033[2K\r"; done
+nproc;while true; do echo -n `ps -eo %cpu,pid --sort -%cpu | awk  '{if($1>90)print}' | wc -l` " "; sleep 1; echo -en "\033[2K\r"; done
 

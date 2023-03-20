d87644a084bebf27e85db9500efdb2bd9fe3463a
Jesse McDonald <30709204+Jesse-McDonald@users.noreply.github.com>
2023-03-20 07:27:30 -0700

    Update server.py

--- run_scripts/network_versions/server.py
+++ run_scripts/network_versions/server.py
@@ -14,7 +14,7 @@ from pymongo import MongoClient
 todoLock=threading.Lock()
 todo = []
 import traceback
-from datetime import datetime
+from datetime import datetime, date
 
 def nextAST(s):
 	data = ""
@@ -71,7 +71,7 @@ def readyNext(mydb, remove=True):
 	
 ingestLock=threading.Lock()
 def connectionThread(c,addr,mydb):
-	print("[", datetime.now().strftime("%H:%M:%S"),']', end=' ')
+	print("[",date.today(), datetime.now().strftime("%H:%M:%S"),']', end=' ')
 	try:
 			print('Got connection from', addr)
 			rec = nextAST(c)

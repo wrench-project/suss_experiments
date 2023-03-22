2766c563ebdf5908db891d66e44e971cfe076785
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2023-03-21 14:35:23 -1000

    Update theNotQuiteAsHackyMonitor.sh

--- run_scripts/network_versions/theNotQuiteAsHackyMonitor.sh
+++ run_scripts/network_versions/theNotQuiteAsHackyMonitor.sh
@@ -1,2 +1,2 @@
-while true; do echo -n `nproc` " ";echo -n `ps -eo %cpu,pid --sort -%cpu | awk  '{if($1>90)print}' | wc -l` " "; done
+while true; do echo $(echo -n `nproc` " ";echo -n `ps -eo %cpu,pid --sort -%cpu | awk  '{if($1>90)print}' | wc -l` " "); done
 

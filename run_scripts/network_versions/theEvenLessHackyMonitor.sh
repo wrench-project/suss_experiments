c4cb035a4552f7fa7787d3cd54aa23836a461231
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2023-04-04 14:48:22 -1000

    Update theEvenLessHackyMonitor.sh

--- run_scripts/network_versions/theEvenLessHackyMonitor.sh
+++ run_scripts/network_versions/theEvenLessHackyMonitor.sh
@@ -1,1 +1,1 @@
-echo -n $(echo -n `nproc` " ";echo -n `ps -eo %cpu,pid --sort -%cpu | awk  '{if($1>90)print}' | wc -l` " ") " "
+echo -n `nproc` " ";echo -n `ps -eo %cpu,pid --sort -%cpu | awk  '{if($1>90)print}' | wc -l` " "

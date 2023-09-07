7841f394eabb50825203aaf4ec405c29a79e7d32
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2023-09-06 15:53:44 -1000

    Update plot_noise_mitigation.py

--- plot_analyze_results_scripts/plot_noise_mitigation.py
+++ plot_analyze_results_scripts/plot_noise_mitigation.py
@@ -236,7 +236,7 @@ if __name__ == "__main__":
 		sys.stderr.write("Usage: " + sys.argv[0] + " <version>\n")
 		sys.exit(1)
 	file_factor=1
-	plot_path, result_dicts, workflows, clusters, best_algorithm_on_average = importData(sys.argv[1],file_factor. 1)
+	plot_path, result_dicts, workflows, clusters, best_algorithm_on_average = importData(sys.argv[1],file_factor, 1)
 	# Generate the noise violin plots for all cases
 	sys.stdout.write("\n# ERROR PLOTS\n")
 	sys.stdout.write("#############\n")
